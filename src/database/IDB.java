ad9e2a69bca8fc78812cae5c0e942ccc13bc20b7
Alizhan Manap <254732@astanait.edu.kz>
2026-02-02 22:27:31 +0500

    Alizhan Manap

--- src/database/IDB.java
+++
@@ -1,7 +0,0 @@
-package database;
-
-import java.sql.Connection;
-
-public interface IDB {
-    Connection getConnection();
-}
--- src/exceptions/InvalidDriverAgeException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class InvalidDriverAgeException extends RuntimeException {
-    public InvalidDriverAgeException(String message) {
-        super(message);
-    }
-}
