80d3d71f224c305b7b0b27f2a6a373e04ed7ff3a
Alizhan Manap <254732@astanait.edu.kz>
2026-02-04 15:41:29 +0500

    Alizhan Manap - implemented Singleton for InsurancePolicy

---
+++ src/models/InsurancePolicy.java
@@ -0,0 +1,29 @@
+package models;
+
+public class InsurancePolicy {
+
+    private static InsurancePolicy instance;
+
+    private double coverageAmount;
+    private double dailyCost;
+
+    private InsurancePolicy() {
+        this.coverageAmount = 50000.0;
+        this.dailyCost = 15.0;
+    }
+
+    public static InsurancePolicy getInstance() {
+        if (instance == null) {
+            instance = new InsurancePolicy();
+        }
+        return instance;
+    }
+
+    public double getDailyCost() {
+        return dailyCost;
+    }
+
+    public double getCoverageAmount() {
+        return coverageAmount;
+    }
+}
