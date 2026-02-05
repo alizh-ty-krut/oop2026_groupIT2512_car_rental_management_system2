9a50e10b1c99b919b5b31755a1b1f962fbc6129d
Ice <aisultan2007q.gmail.com git config --list fwfew>
2026-02-05 14:20:55 +0500

    Aisultan Beiimbet - add RentalFactory

---
+++ src/factory/RentalFactory.java
@@ -0,0 +1,18 @@
+package factory;
+
+import java.time.LocalDate;
+import models.Rental;
+
+public final class RentalFactory {
+
+    private RentalFactory() {
+    }
+
+    public static Rental createRental(Integer id, int carId, int customerId, LocalDate date) {
+        return new Rental(id, carId, customerId, date);
+    }
+
+    public static Rental createRental(Integer id, int carId, int customerId) {
+        return new Rental(id, carId, customerId, LocalDate.now());
+    }
+}
