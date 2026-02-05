8c5e119d5a1d5a21dec5be7131adc651913c93e3
Alizhan Manap <254732@astanait.edu.kz>
2026-02-05 13:48:25 +0500

    Alizhan Manap - fixed Rental

--- src/models/Rental.java
+++ src/models/Rental.java
@@ -20,18 +20,21 @@
 package models;
 
 import java.time.LocalDate;
+import models.InsurancePolicy;
 
 public class Rental {
     private Integer id;
     private int carId;
     private int customerId;
     private LocalDate rentalDate;
+    private InsurancePolicy insurancePolicy;
 
     public Rental(Integer id, int carId, int customerId, LocalDate rentalDate) {
         setId(id);
         setCarId(carId);
         setCustomerId(customerId);
         setRentalDate(rentalDate);
+        this.insurancePolicy = InsurancePolicy.getInstance();
     }
 
     public Integer getId() { return id; }
@@ -46,4 +49,7 @@ public class Rental {
     public LocalDate getRentalDate() { return rentalDate; }
     public void setRentalDate(LocalDate rentalDate) { this.rentalDate = rentalDate; }
 
+    public double getInsuranceCost() {
+        return insurancePolicy.getDailyCost();
+    }
 }
